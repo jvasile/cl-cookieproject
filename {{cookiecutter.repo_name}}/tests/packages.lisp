05029b691e63af35ee125189aa9eeba44c2f2ac4
Jin-Ho King <kjinho@users.noreply.github.com>
2024-02-28 19:27:45 -0500

    Delete {{cookiecutter.repo_name}}/tests/packages.lisp

--- {{cookiecutter.repo_name}}/tests/packages.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :asdf-user)
-(defpackage :{{ cookiecutter.project_name }}-tests
-  (:use :common-lisp
-        :fiveam
-        :{{ cookiecutter.project_name }}))
-
-
-(in-package :{{ cookiecutter.project_name }}-tests)
