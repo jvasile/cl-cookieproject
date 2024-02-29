694abca098ec3c09a32ea02f05309ceb69df276d
Jin-Ho King <kjinho@users.noreply.github.com>
2024-02-28 19:28:59 -0500

    Update test-{{cookiecutter.project_name}}.lisp

--- {{cookiecutter.repo_name}}/tests/test-{{cookiecutter.project_name}}.lisp
+++ {{cookiecutter.repo_name}}/tests/test-{{cookiecutter.project_name}}.lisp
@@ -1,4 +1,8 @@
-(in-package :{{ cookiecutter.project_name }}-tests)
+(defpackage :{{ cookiecutter.project_name }}-tests/tests/{{ cookiecutter.project_name }}-tests
+  (:use :common-lisp
+        :fiveam
+        :{{ cookiecutter.project_name }}))
+(in-package :{{ cookiecutter.project_name }}-tests/tests/{{ cookiecutter.project_name }}-tests)
 
 ;; Define your project tests here...
 
