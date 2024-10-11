56112eb6219b8e1196db0df29d64c41033885b13
James Vasile <james@jamesvasile.com>
2024-10-10 23:42:21 -0400

    Hush quickload

--- {{cookiecutter.repo_name}}/run.lisp
+++ {{cookiecutter.repo_name}}/run.lisp
@@ -15,7 +15,7 @@ Another solution to run the app is to build and run a binary (see README).
 
 (load "{{ cookiecutter.project_name}}.asd")
 
-(ql:quickload "{{ cookiecutter.project_name }}")
+(ql:quickload "{{ cookiecutter.project_name }}" :silent t)
 
 (in-package :{{ cookiecutter.project_name}}/src/main)
 (handler-case
